013e3d5b89e5071cc2cd428ad8ee808bebc6807f
lchirata <larissa.c.hirata@gmail.com>
2020-09-06 18:29:52 -0300

    config.js

---
+++ database/configs.js
@@ -0,0 +1,11 @@
+module.exports = {
+    username:'b4a3c77c85fd11',
+    password:'09a28314',
+    dialect:'mysql',
+    database:'heroku_0bf27128c496b81',
+    host:'us-cdbr-east-02.cleardb.com',
+    define : {
+        timestamps: false,
+        underscored: true,
+    }
+}
